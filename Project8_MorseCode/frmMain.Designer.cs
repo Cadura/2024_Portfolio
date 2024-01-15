deb9932ba10dcbe943979a801b335fa0a2a6be05
Chris Duran <156707471+Cadura@users.noreply.github.com>
2024-01-15 14:13:49 -0600

    Add files via upload

---
+++ Project8_MorseCode/frmMain.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Project8_Morse_Code
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEnterMsg = new System.Windows.Forms.Label();
+            this.lblMorCode = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.lblOutCode = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblEnterMsg
+            // 
+            this.lblEnterMsg.AutoSize = true;
+            this.lblEnterMsg.Location = new System.Drawing.Point(34, 49);
+            this.lblEnterMsg.Name = "lblEnterMsg";
+            this.lblEnterMsg.Size = new System.Drawing.Size(101, 16);
+            this.lblEnterMsg.TabIndex = 2;
+            this.lblEnterMsg.Text = "Enter Message:";
+            // 
+            // lblMorCode
+            // 
+            this.lblMorCode.AutoSize = true;
+            this.lblMorCode.Location = new System.Drawing.Point(34, 107);
+            this.lblMorCode.Name = "lblMorCode";
+            this.lblMorCode.Size = new System.Drawing.Size(84, 16);
+            this.lblMorCode.TabIndex = 3;
+            this.lblMorCode.Text = "Morse Code:";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMessage.Location = new System.Drawing.Point(147, 44);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(480, 24);
+            this.txtMessage.TabIndex = 1;
+            this.txtMessage.TextChanged += new System.EventHandler(this.txtMessage_TextChanged);
+            // 
+            // lblOutCode
+            // 
+            this.lblOutCode.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblOutCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutCode.Location = new System.Drawing.Point(147, 107);
+            this.lblOutCode.Name = "lblOutCode";
+            this.lblOutCode.Size = new System.Drawing.Size(480, 150);
+            this.lblOutCode.TabIndex = 4;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.Location = new System.Drawing.Point(254, 291);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(114, 38);
+            this.btnConvert.TabIndex = 5;
+            this.btnConvert.Text = "C&onvert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(374, 291);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(114, 38);
+            this.btnReset.TabIndex = 6;
+            this.btnReset.Text = "R&eset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(494, 291);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(114, 38);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 362);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.lblOutCode);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.lblMorCode);
+            this.Controls.Add(this.lblEnterMsg);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Morse Code Converter by Cho";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEnterMsg;
+        private System.Windows.Forms.Label lblMorCode;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label lblOutCode;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Project8_MorseCode/frmMain.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project8_Morse_Code
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        #region "properties"
+        //properties
+        char[] morseCharacters =
+        {
+            ' ', ',', '.', '?', '0', '1', '2', '3', '4', '5', '6', '7', '8',
+            '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+            'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+        };
+
+        string[] morseCode =
+        {
+            "   ", "--..--", ".-.-.-", "..--..", "-----", ".----", "..---", "...--",
+            "....-", ".....", "-....", "--...", "---..", "----.", ".-", "-...", "-.-.", "-..",
+            ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.",
+            "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."
+        };
+        #endregion
+
+        #region "Exit Button"
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        #endregion
+
+        #region "Convert Button"
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            string userMessaage = txtMessage.Text; //string for user's input message
+            userMessaage = userMessaage.ToUpper(); //convert input message to uppercase
+            char[] ch = userMessaage.ToCharArray(); //store user's input message to char array
+            string strResult = ""; //empty string to store the converted result
+
+
+            for (int x = 0; x < ch.Length; x++) //loop through the ch array (user's input)
+            {
+                for (int y = 0; y < morseCharacters.Length; y++) //morseCharacter array
+                {
+                    if (morseCharacters[y] == ch[x]) //compare and match the character array with user input array
+                    {
+                        strResult = morseCode[y]; //assign the morse code array to string result                        }
+                    }
+                }
+
+                lblOutCode.Text += strResult + ' '; //display the morse code
+
+            }
+
+        }
+        #endregion
+
+        #region "Reset Button"
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtMessage.Text = "";
+            lblOutCode.Text = "";
+            txtMessage.Focus();
+        }
+
+
+
+        #endregion
+
+        #region "Keypressed in Textbox"
+        private void txtMessage_TextChanged(object sender, EventArgs e)
+        {
+            lblOutCode.Text = "";
+        }
+        #endregion
+
+    }
+}
