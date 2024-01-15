8082b715b554c212a409ecf34d271378d3b07d54
Chris Duran <156707471+Cadura@users.noreply.github.com>
2024-01-15 16:34:08 -0600

    Add files via upload

---
+++ Project 10 - Structures(EmployeeIDs and Salary)/MainForm.Designer.vb
@@ -0,0 +1,104 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.listIds = New System.Windows.Forms.ListBox()
+        Me.labEmployees = New System.Windows.Forms.Label()
+        Me.labSalary = New System.Windows.Forms.Label()
+        Me.outputSalary = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'listIds
+        '
+        Me.listIds.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.listIds.FormattingEnabled = True
+        Me.listIds.ItemHeight = 18
+        Me.listIds.Location = New System.Drawing.Point(15, 53)
+        Me.listIds.Name = "listIds"
+        Me.listIds.Size = New System.Drawing.Size(107, 130)
+        Me.listIds.TabIndex = 0
+        '
+        'labEmployees
+        '
+        Me.labEmployees.AutoSize = True
+        Me.labEmployees.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.labEmployees.Location = New System.Drawing.Point(19, 28)
+        Me.labEmployees.Name = "labEmployees"
+        Me.labEmployees.Size = New System.Drawing.Size(104, 18)
+        Me.labEmployees.TabIndex = 1
+        Me.labEmployees.Text = "&Employee IDs:"
+        '
+        'labSalary
+        '
+        Me.labSalary.AutoSize = True
+        Me.labSalary.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.labSalary.Location = New System.Drawing.Point(145, 28)
+        Me.labSalary.Name = "labSalary"
+        Me.labSalary.Size = New System.Drawing.Size(53, 18)
+        Me.labSalary.TabIndex = 1
+        Me.labSalary.Text = "Salary:"
+        '
+        'outputSalary
+        '
+        Me.outputSalary.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.outputSalary.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.outputSalary.Location = New System.Drawing.Point(148, 64)
+        Me.outputSalary.Name = "outputSalary"
+        Me.outputSalary.Size = New System.Drawing.Size(93, 30)
+        Me.outputSalary.TabIndex = 1
+        Me.outputSalary.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(146, 135)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(94, 35)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(274, 213)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.outputSalary)
+        Me.Controls.Add(Me.labSalary)
+        Me.Controls.Add(Me.labEmployees)
+        Me.Controls.Add(Me.listIds)
+        Me.Name = "MainForm"
+        Me.Text = "Zander Inc."
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents listIds As ListBox
+    Friend WithEvents labEmployees As Label
+    Friend WithEvents labSalary As Label
+    Friend WithEvents outputSalary As Label
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Project 10 - Structures(EmployeeIDs and Salary)/MainForm.vb
@@ -0,0 +1,89 @@
+﻿Public Class MainForm
+
+    '/*******************************
+    ' 
+    '  Program: Converting Distances
+    '
+    '  Author: Chris D.
+    '  Description: Using structures and files to display data.
+    ' 
+    ' *****************************
+
+
+    'declare structure
+    Structure Employees
+        'structure variables to use for each employee
+        Public strId As String  ' Employee has an ID
+        Public dblSalary As Double ' Employee has a salary
+    End Structure
+
+    ' declare array of structure variables - structure array variable is an object now
+    Private employeeList(4) As Employees 'The items in the array are Employees 1 through 4. 
+    ' EmployeeList is an object
+
+
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Declarations
+        Dim fileTxtString As String = String.Empty
+        Dim i As Integer = 0
+
+        'Declare object first
+        Dim inputFile As IO.StreamReader ' taking a class and placing it as a variable/object
+
+
+        ' Check so see file exists
+        If IO.File.Exists("Employees.txt") Then
+            ' Initialize object next
+            inputFile = IO.File.OpenText("Employees.txt")
+
+            'Read lines from text file until end of file or when the employeeList array is full
+            Do Until inputFile.Peek = -1
+
+                ' take that line from text file, and store in a variable 
+                fileTxtString = inputFile.ReadLine
+
+                ' move that variable into the employeelist array
+                employeeList(i).strId = fileTxtString.Substring(0, 5) ' grab each character starting from 0 position and span to 5 characters from string variable
+
+                ' grab salary from current line and store in array
+                Double.TryParse(fileTxtString.Substring(6), employeeList(i).dblSalary) ' grab characters from position 6 to rest of string.
+
+                'add the id from structure array to listbox
+                listIds.Items.Add(employeeList(i).strId)
+
+                'increment to next Employeelist item
+                i += 1
+
+            Loop
+
+            'close file
+            inputFile.Close()
+        Else ' Doesn't exist
+            MessageBox.Show("Can't find the file",
+                            "Game Show Contestants",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information)
+        End If
+
+
+
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub listIds_SelectedIndexChanged(sender As Object, e As EventArgs) Handles listIds.SelectedIndexChanged
+        ' When any listbox items are selected, the output-label control for salary will grab 
+        ' from the structure array - the "salary" stucture variable
+        ' it does this by selecting the position of the array based on what the user clicked from
+        ' the listbox control - top of listbox is index 0, bottom is index 4
+
+        ' Once index is chosen, it picks the 'salary' variable and converts it to string in currency format
+
+        outputSalary.Text = employeeList(listIds.SelectedIndex).dblSalary.ToString("c0")
+
+    End Sub
+End Class
