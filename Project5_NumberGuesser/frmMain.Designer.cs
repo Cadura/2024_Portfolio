3348e5ad4a3e65032f3d79a1af05e150b8c35702
Chris Duran <156707471+Cadura@users.noreply.github.com>
2024-01-15 14:06:19 -0600

    Add files via upload

---
+++ Project5_NumberGuesser/frmMain.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Project5_NumberGuesser
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEnterNumber = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.txtEnterNumber = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblGuessedNum = new System.Windows.Forms.Label();
+            this.lblGuessedResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblEnterNumber
+            // 
+            this.lblEnterNumber.AutoSize = true;
+            this.lblEnterNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnterNumber.Location = new System.Drawing.Point(110, 42);
+            this.lblEnterNumber.Name = "lblEnterNumber";
+            this.lblEnterNumber.Size = new System.Drawing.Size(195, 20);
+            this.lblEnterNumber.TabIndex = 0;
+            this.lblEnterNumber.Text = "Enter your guess (1-100)";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(114, 109);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(91, 30);
+            this.btnCheck.TabIndex = 1;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // txtEnterNumber
+            // 
+            this.txtEnterNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEnterNumber.Location = new System.Drawing.Point(139, 69);
+            this.txtEnterNumber.Name = "txtEnterNumber";
+            this.txtEnterNumber.Size = new System.Drawing.Size(147, 34);
+            this.txtEnterNumber.TabIndex = 0;
+            this.txtEnterNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(214, 109);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(91, 30);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblGuessedNum
+            // 
+            this.lblGuessedNum.AutoSize = true;
+            this.lblGuessedNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGuessedNum.Location = new System.Drawing.Point(111, 155);
+            this.lblGuessedNum.Name = "lblGuessedNum";
+            this.lblGuessedNum.Size = new System.Drawing.Size(137, 18);
+            this.lblGuessedNum.TabIndex = 3;
+            this.lblGuessedNum.Text = "You have guessed: ";
+            // 
+            // lblGuessedResult
+            // 
+            this.lblGuessedResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGuessedResult.Location = new System.Drawing.Point(251, 155);
+            this.lblGuessedResult.Name = "lblGuessedResult";
+            this.lblGuessedResult.Size = new System.Drawing.Size(60, 23);
+            this.lblGuessedResult.TabIndex = 4;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 218);
+            this.Controls.Add(this.lblGuessedResult);
+            this.Controls.Add(this.txtEnterNumber);
+            this.Controls.Add(this.lblGuessedNum);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.lblEnterNumber);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Guess the number!";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEnterNumber;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.TextBox txtEnterNumber;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblGuessedNum;
+        private System.Windows.Forms.Label lblGuessedResult;
+    }
+}
+
---
+++ Project5_NumberGuesser/frmMain.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+/*******************************
+ * 
+ *  Program: Number Guesser
+ *  Date: 02/07/2022
+ *  Author: Chris D.
+ *  Description: A number guesser game where a computer 
+ *  generates a random number. The user will guess
+ *  a number, and the computer will respond higher or lower 
+ *  until it hits the mark.
+ * 
+ * *****************************/
+namespace Project5_NumberGuesser
+{
+    public partial class frmMain : Form
+    {
+        //variables
+        int number = 0;
+        int guess = 0;
+        int guessedNum = 0;
+
+        //create random number
+        Random randNum = new Random();
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            guess = Convert.ToInt32(txtEnterNumber.Text); //convert the string to number
+
+            if (guess == number) //when the guess number is correct
+            {
+                MessageBox.Show("You've guessed it!");
+                txtEnterNumber.Text = "";
+                guessedNum += 1; //increment the guessed number
+                lblGuessedResult.Text = guessedNum.ToString(); //display guessed number
+                //reset the number
+                Random randNum = new Random();
+                number = randNum.Next(1, 100);
+                txtEnterNumber.Focus();
+                txtEnterNumber.SelectAll();
+
+
+            }
+            else if (guess < number) //when the guess number is lower
+            {
+                MessageBox.Show("HIGHER!");
+                txtEnterNumber.Focus();
+                txtEnterNumber.SelectAll();
+            }
+            else //when the guess number is higher
+            {
+                MessageBox.Show("LOWER!");
+                txtEnterNumber.Focus();
+                txtEnterNumber.SelectAll();
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            //random integer from 1 to 100
+            number = randNum.Next(1, 100);
+        }
+    }
+}
